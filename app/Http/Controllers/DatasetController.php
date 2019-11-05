0283cb59563c70b01774edd2c1fc8fdfa17f4751
Henry Addison <henryaddison@users.noreply.github.com>
2019-11-05 16:02:18 +0000

    move dataset site into own controller

---
+++ app/Http/Controllers/DatasetController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use OpenActive\DatasetSiteTemplate\FeedType;
+use OpenActive\DatasetSiteTemplate\TemplateRenderer;
+
+class DatasetController extends Controller
+{
+    public function show()
+    {
+        $supportedFeedTypes = array(
+            FeedType::SESSION_SERIES,
+        );
+
+        $settings = array(
+            "openDataFeedBaseUrl" => "https://sw-oa-test-site.herokuapp.com/feed/",
+            "datasetSiteUrl" => "https://sw-oa-test-site.herokuapp.com",
+            "datasetDiscussionUrl" => "https://github.com/simpleweb/sw-oa-php-test-site",
+            "datasetDocumentationUrl" => "https://developer.openactive.io/",
+            "datasetLanguages" => array("en-GB"),
+            "organisationName" => "Simpleweb",
+            "organisationUrl" => "https://www.simpleweb.co.uk/",
+            "organisationLegalEntity" => "Simpleweb Ltd",
+            "organisationPlainTextDescription" => "Simpleweb is a purpose driven software company that specialises in new technologies, product development, and human interaction.",
+            "organisationLogoUrl" => "https://simpleweb.co.uk/wp-content/uploads/2015/07/facebook-default.png",
+            "organisationEmail" => "spam@simpleweb.co.uk",
+            "backgroundImageUrl" => "https://simpleweb.co.uk/wp-content/uploads/2017/06/IMG_8994-500x500-c-default.jpg",
+            "dateFirstPublished" => "2019-11-05", // remember, remember the fifth of November...
+        );
+
+        echo((new TemplateRenderer())->renderSimpleDatasetSite($settings, $supportedFeedTypes));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,34 +11,4 @@
 |
 */
 
-use OpenActive\DatasetSiteTemplate\FeedType;
-use OpenActive\DatasetSiteTemplate\TemplateRenderer;
-
-Route::get('/', function () {
-    $supportedFeedTypes = array(
-        FeedType::FACILITY_USE,
-        FeedType::SCHEDULED_SESSION,
-        FeedType::SESSION_SERIES,
-        FeedType::SLOT,
-    );
-
-    $settings = array(
-        "openDataFeedBaseUrl" => "https://sw-oa-test-site.herokuapp.com/feed/",
-        "datasetSiteUrl" => "https://sw-oa-test-site.herokuapp.com",
-        "datasetDiscussionUrl" => "https://github.com/simpleweb/sw-oa-php-test-site",
-        "datasetDocumentationUrl" => "https://developer.openactive.io/",
-        "datasetLanguages" => array("en-GB"),
-        "organisationName" => "Simpleweb",
-        "organisationUrl" => "https://www.simpleweb.co.uk/",
-        "organisationLegalEntity" => "Simpleweb Ltd",
-        "organisationPlainTextDescription" => "Simpleweb is a purpose driven software company that specialises in new technologies, product development, and human interaction.",
-        "organisationLogoUrl" => "https://simpleweb.co.uk/wp-content/uploads/2015/07/facebook-default.png",
-        "organisationEmail" => "spam@simpleweb.co.uk",
-        "backgroundImageUrl" => "https://simpleweb.co.uk/wp-content/uploads/2017/06/IMG_8994-500x500-c-default.jpg",
-        "dateFirstPublished" => "2019-11-05", // remember, remember the fifth of November...
-    );
-
-    echo((new TemplateRenderer())->renderSimpleDatasetSite($settings, $supportedFeedTypes));
-
-    // return view('welcome');
-});
+Route::get('/', 'DatasetController@show');
